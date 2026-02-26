8e14ab8a7bd71ddb4baa8be02d6b41c2557e66ea
Anokhi <139269672+Anokhi-hirsch@users.noreply.github.com>
2026-02-26 07:53:47 -0800

    Update 595.BigCountries.sql

--- SQL Leetcode/595.BigCountries.sql
+++ SQL Leetcode/595.BigCountries.sql
@@ -1,1 +1,12 @@
+-- leetcode 595.Big countries
 
+--Using OR
+SELECT name,population,area FROM World WHERE area>= 3000000 OR population>= 25000000;
+
+--Using Union
+SELECT name,population,area FROM World WHERE area>= 3000000
+UNION
+SELECT name,population,area FROM World WHERE population>= 25000000;
+
+-- which one is better
+-- 
