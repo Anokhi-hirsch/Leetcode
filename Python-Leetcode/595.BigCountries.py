3d9e4a70f3990232e2217aef66bfdee6c033e2ea
Anokhi <139269672+Anokhi-hirsch@users.noreply.github.com>
2026-02-26 09:04:17 -0800

    Update 595.BigCountries.py

--- Python-Leetcode/595.BigCountries.py
+++ Python-Leetcode/595.BigCountries.py
@@ -4,3 +4,9 @@ df big_countries(world:pd.Dataframe:
   return world[(world['area']>=3000000)|(world['population']>=25000000)]
 [['name'],['population'],['area']]
 
+# another way
+import panda as pd
+df big_countries(world:pd.Dataframe:
+big_countries_df=world[(world['area']>=3000000)|(world['population']>=25000000)]
+result_df=big_countries_df[['name'],['population'],['area']]
+return result_df
