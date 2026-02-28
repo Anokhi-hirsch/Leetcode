3bc67419ff591d8abf35147bff7744e3595fa465
Anokhi <139269672+Anokhi-hirsch@users.noreply.github.com>
2026-02-27 16:02:41 -0800

    solved 595.BigCountries.py

--- Python-Leetcode/595.BigCountries.py
+++ Python-Leetcode/595.BigCountries.py
@@ -1,4 +1,8 @@
 # leeetcode 595 Big countries
+import panda as pd
+df big_countries(world:pd.Dataframe:
+  return world[(world['area']>=3000000)|(world['population']>=25000000)]
+[['name'],['population'],['area']]
 import pandas as pd
 
 def big_countries(world:pd.DataFrame:
@@ -8,6 +12,11 @@ def big_countries(world:pd.DataFrame:
     ][['name', 'population', 'area']]
 
 # another way
+import panda as pd
+df big_countries(world:pd.Dataframe:
+big_countries_df=world[(world['area']>=3000000)|(world['population']>=25000000)]
+result_df=big_countries_df[['name'],['population'],['area']]
+return result_df
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
