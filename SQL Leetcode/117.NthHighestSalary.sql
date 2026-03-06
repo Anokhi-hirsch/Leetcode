e6835d318ef85c367a29e9248a3370aaf4a1a373
Anokhi <139269672+Anokhi-hirsch@users.noreply.github.com>
2026-03-06 10:23:55 -0800

    Update 117.NthHighestSalary.sql

--- SQL Leetcode/117.NthHighestSalary.sql
+++ SQL Leetcode/117.NthHighestSalary.sql
@@ -1,1 +1,12 @@
-
+--117 nth highest salary
+--using window function dense_rank and CTE 
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+  --creating temporary table CTE to rank salaries , using dense_rank window function
+  with cte as (
+  select *, Dense_rank() over (order by salary desc) as rnk from Employee
+  )
+  select distinct ifnull(salary,null) from cte where rnk=N --selecting distinct salary and if null then give back null
+);
+END
